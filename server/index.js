8fa7f553d590b05e19f6a5616615f25d84ab2922
becopro <becopro@users.noreply.github.com>
2026-08-05 14:26:17 -0300

    fix(server): change default port to 80 for production hosting

--- server/index.js
+++ server/index.js
@@ -228,7 +228,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 server.listen(PORT, () => {
     console.log('[Server] Running on port ' + PORT);
 });
